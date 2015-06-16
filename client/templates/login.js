c39881902b962735da3b43f43b33b62eab523647
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-06-16 15:06:09 +0530

    Adds register functionality and reformats login.js

--- client/templates/login.js
+++ client/templates/login.js
@@ -1,41 +1,51 @@
 Template.login.events({
-	'click .fbBtn': function(){
-		Meteor.loginWithFacebook();
-	},
-	'click .gBtn': function(){
-		Meteor.loginWithGoogle();
-	},
-	'submit #login-form': function(event, template){
-		event.preventDefault();
-		var passwordElement = template.find("#login-password"),
-			usernameElement = template.find("#login-username");
-		var username = trimInput(usernameElement.value),
-			password = passwordElement.value;
-		if(username.length)
-			if(isValidPassword(password))
-				Meteor.loginWithPassword(username, password, function(err){
-					if(err)
-						Materialize.toast(err.reason, 2000);
-					else
-						Router.go('projects');
-				})
-			else{
-				$(passwordElement).removeClass('invalid').addClass('invalid');
-				if(password.length)
-					Materialize.toast('Minimum 6 characters required!', 2000)
-				else
-					Materialize.toast('Password is required!', 2000);
-			}
-		else{
-			$(usernameElement).removeClass('invalid').addClass('invalid');
-			Materialize.toast('Username is required!', 2000)
-		}
-		return false;
-	}
-})
-var trimInput = function(val) {
-	return val.replace(/^\s*|\s*$/g, "");
-}
-var isValidPassword = function(val) {
-	return val.length >= 6;
-}
+    'click .fbBtn': function () {
+        Meteor.loginWithFacebook();
+    },
+    'click .gBtn': function () {
+        Meteor.loginWithGoogle();
+    },
+    'submit #login-form': function (event, template) {
+        event.preventDefault();
+        var passwordElement = template.find("#login-password"),
+            emailElement = template.find("#login-email");
+        var email = trimInput(emailElement.value),
+            password = passwordElement.value;
+        if(email.length)
+            if (isValidPassword(password)){
+                var currentSubmitButton = event.originalEvent.explicitOriginalTarget.id;
+                if(currentSubmitButton=="login-btn")
+                    Meteor.loginWithPassword(email, password, function (err) {
+                        if (err)
+                            Materialize.toast(err.reason, 2000);
+                        else
+                            Router.go('projects');
+                    });
+                else if (currentSubmitButton == "register-btn")
+                    Accounts.createUser({email: email, password: password}, function (err) {
+                        if (err)
+                            Materialize.toast(err.reason, 2000);
+                        else
+                            Router.go('projects');
+                    });
+            }
+            else {
+                $(passwordElement).removeClass('invalid').addClass('invalid');
+                if (password.length)
+                    Materialize.toast('Minimum 6 characters required!', 2000)
+                else
+                    Materialize.toast('Password is required!', 2000);
+            }
+        else {
+            $(emailElement).removeClass('invalid').addClass('invalid');
+            Materialize.toast('Email is required!', 2000);
+        }
+        return false;
+    }
+});
+var trimInput = function (val) {
+    return val.replace(/^\s*|\s*$/g, "");
+};
+var isValidPassword = function (val) {
+    return val.length >= 6;
+};
