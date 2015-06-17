e4a01d251f2c83d34d31181a33398173bef87e40
deepank agarwal <deepank411@gmail.com>
2015-06-17 22:19:31 +0530

    Refactored code, added selective new tag

--- client/templates/projectsList.js
+++ client/templates/projectsList.js
@@ -1,8 +1,16 @@
 Template.projectsList.helpers({
-	unreadCount: function(){
-		return this.unreadCount();
-	},
+	// unreadCount: function(){
+	// 	return this.unreadCount();
+	// },
 	orders: function(){
 		return this.orders();
+	},
+	countunread: function() {
+	if(this.unreadCount !== 0) {
+		return this.unreadCount();
 	}
+	else{
+		return false;
+	}
+}
 });
