c5e819f1cd61f54c9e958a79f1bf4443a4dfc10a
pikachuPokemon <akshay.kmr@outlook.com>
2015-06-18 18:33:42 +0530

    test notifications working. open notificationReadme

---
+++ client/templates/notificationListner.js
@@ -0,0 +1,19 @@
+Push.addListener('message', function(notification) {
+	// Called on every message
+	//if you want to add some callbacks upon recieving notifications this is the place
+
+	if (Meteor.isCordova) {
+		window.alert = navigator.notification.alert;
+	}
+
+
+	/*below is an example - not used in app*/
+	/*function alertDismissed() {
+		NotificationHistory.update({_id: notification.payload.historyId}, {
+			$set: {
+				"recievedAt": new Date()
+			}
+		});
+	}
+	alert(notification.message, alertDismissed , notification.payload.title, "Ok");*/
+});
