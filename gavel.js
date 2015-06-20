2e2b5725a1e6ef3e31948e53153e4a4a52dc479e
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-06-20 09:48:53 +0530

    Implements file opener plugin to download an open the pdf file

--- gavel.js
+++ gavel.js
@@ -1,97 +1,120 @@
 if (Meteor.isClient) {
 
-  Template.nav.rendered = function() {
-    // Initialize collapse button
-    $(".button-collapse").sideNav();
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    //$('.collapsible').collapsible();
-  }
-
-  Template.addProject.rendered = function(){
-    $('select').material_select();
-  }
-
-  Template.addProject.events({
-    'submit form': function( event ){
-      debugger;
-      event.preventDefault();
-      Projects.insert({
-        'title' : event.target.children[0].children[0].children[0].value,
-        'ctype' : event.target.children[0].children[1].children[0].children[3].value,
-        'cno' : event.target.children[0].children[2].children[0].value,
-        'cyear' : event.target.children[0].children[3].children[0].children[3].value,
-        'userId' : Meteor.userId()
-      });
-      Router.go('projects');
+    Template.nav.rendered = function () {
+        // Initialize collapse button
+        $(".button-collapse").sideNav();
+        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+        //$('.collapsible').collapsible();
     }
-  }); 
 
-  Template.projectsList.helpers({
-    'projects' : function(){
-      return Projects.find();
+    Template.addProject.rendered = function () {
+        $('select').material_select();
     }
-  });
 
-  Template.projectsList.events({
-    'click .projectRow' : function(event, template){
-      debugger;
-      Router.go('detailsProject',{_id: this._id});     
-    }
-  })
+    Template.addProject.events({
+        'submit form': function (event) {
+            debugger;
+            event.preventDefault();
+            Projects.insert({
+                'title': event.target.children[0].children[0].children[0].value,
+                'ctype': event.target.children[0].children[1].children[0].children[3].value,
+                'cno': event.target.children[0].children[2].children[0].value,
+                'cyear': event.target.children[0].children[3].children[0].children[3].value,
+                'userId': Meteor.userId()
+            });
+            Router.go('projects');
+        }
+    });
 
-  Template.addButton.events({
-    'click .btn-floating' : function(){
-      Router.go('addProject');
-    }
-  });
+    Template.projectsList.helpers({
+        'projects': function () {
+            return Projects.find();
+        }
+    });
 
-  Template.detailsProject.helpers({
-    'yo' : function(){
-      debugger;
-    },
-    'orders' : function(){
-      return this.orders();
-    },
-    'orderFetched' : function(){
-      if (this.orders().length > 0){
-        return true;
-      }else{
-        return false;
-      }
-    }
-  });
+    Template.projectsList.events({
+        'click .projectRow': function (event, template) {
+            debugger;
+            Router.go('detailsProject', {_id: this._id});
+        }
+    })
 
-  Template.nav.rendered = function() { 
-      $('.dropdown-button').dropdown({
-        inDuration: 300,
-        outDuration: 225,
-        constrain_width: false, // Does not change width of dropdown to that of the activator
-        hover: true, // Activate on hover
-        gutter: 0, // Spacing from edge
-        belowOrigin: false // Displays dropdown below the button
-      });
-      
-  }
+    Template.addButton.events({
+        'click .btn-floating': function () {
+            Router.go('addProject');
+        }
+    });
 
-  Template.nav.events({
-    'click .logout' : function(){
-      Meteor.logout();
-    },
-    'click .tour' : function(){
-      Router.go('tour');
-    }
-  }) 
+    Template.detailsProject.helpers({
+        'yo': function () {
+            debugger;
+        },
+        'orders': function () {
+            return this.orders();
+        },
+        'orderFetched': function () {
+            if (this.orders().length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+
+    Template.nav.rendered = function () {
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false // Displays dropdown below the button
+        });
+
+    };
+
+    Template.nav.events({
+        'click .logout': function () {
+            Meteor.logout();
+        },
+        'click .tour': function () {
+            Router.go('tour');
+        }
+    });
+    Template.detailsProject.events({
+        'click .order-link': function () {
+            if (Meteor.isCordova) {
+                var url = this.link;
+                var filename = url.substring(url.lastIndexOf('/') + 1);
+                var store = cordova.file.dataDirectory;
+                var appStart = function () {
+                    cordova.plugins.fileOpener2.open(store + filename, 'application/pdf', {
+                        error: function (e) {
+                            if (e.message.indexOf("Activity not found") > -1) {
+                                if (confirm("Sorry, no application available to open this file. Confirm to open with google pdf viewer."))
+                                    window.open('https://docs.google.com/viewer?url=' + url, '_system');
+                            } else if (e.message.indexOf("File not found") > -1)
+                                Materialize.toast("File is missing", 1000);
+                            else
+                                Materialize.toast("Some error occured", 1000);
+                        }
+                    });
+                };
+                window.resolveLocalFileSystemURL(store + filename, appStart, function () {
+                    Materialize.toast("Downloading...", 1000);
+                    new FileTransfer().download(url, store + filename, appStart, function (err) {
+                        Materialize.toast("Some error occurred in downloading file", 1000);
+                    });
+                });
+            }
+            else
+                window.open(this.link, '_system');
+        }
+    })
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
-
-
-
-
-
-
-
