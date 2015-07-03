0d229db71a997b7a3b86ec4665f028da26b55bb8
Shashwat Kumar <thegreatshasha@gmail.com>
2015-07-03 08:50:52 +0530

    remove orders on update

--- server/notification.js
+++ server/notification.js
@@ -2,7 +2,7 @@ pushNotify =function(project){
 	var body = {
 		from : 'gavelorders',
 		title: 'New orders have been fetched for',
-		text : 'project: '+ project.title +'case number: '+ project.cno,
+		text : 'project: '+ project.title +' case number: '+ project.cno,
 		query: {userId : project.userId}
 
 	};
